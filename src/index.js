6a5f5996cf0d2754fe3794888dc85481cebb21a3
Marta Evans <vnezapnoanna@gmail.com>
2022-03-09 20:22:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  let sum;
+  if (!matrix) {
+    return [];
+  }
+  for(let i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) {
+      for (let n = matrix[i].length - 1; n >= 0; n--) {
+        sum = matrix[i][n];
+        result.push(sum);
+      }
+    } else {
+      for(let n = 0; n < matrix[i].length; n++) {
+        sum = matrix[i][n];
+        result.push(sum);
+      }
+    }
+  } 
+  return result;
 }
